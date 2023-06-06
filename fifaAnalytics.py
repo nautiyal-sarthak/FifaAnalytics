6fd9c93634f6d3d3069221779fb2073327b31f19
Sarthak <nautiyal.sarthak@gmail.com>
2023-06-05 22:00:35 -0400

    added few graphs to the dashboard

--- fifaAnalytics.py
+++ fifaAnalytics.py
@@ -10,27 +10,63 @@ def read_google_sheet():
     rows = sh.sheet1.get_all_records()
     return rows
 
-def createDatafrme(formatedList):
-    df = pd.DataFrame(formatedList)
-
-    #formating date feilds
+def getDateTimeFeilds(df):
+    # formating date feilds
     # Extract time component into a separate column
     df['Time'] = pd.to_datetime(df['Timestamp']).dt.hour
 
     # Convert 'DateTime' column to datetime type
     df['Date'] = pd.to_datetime(df['Timestamp']).dt.date
+    df = df.drop('Timestamp',axis=1)
+    return df
 
+def getWinner(score,p1,p2):
+    scorelst = score.split("-")
+    winner = "Tied"
+
+    if scorelst[0] > scorelst[1]:
+        winner = p1
+    elif scorelst[0] < scorelst[1]:
+        winner = p2
+
+    return winner
+
+def createDatafrme(formatedList):
+    df = pd.DataFrame(formatedList)
+    return df
+
+def formatDataframe(df):
+    #adding date cols
+    df = getDateTimeFeilds(df)
+
+    #get the winner
+    df['winner'] = df.apply(lambda row: getWinner(row['fulltime score'], row['Home player'], row['Away player']), axis=1)
 
     return df
+
+def getWins(df):
+    wins = df.groupby(['winner', 'Date']).count()['Home team'].reset_index()\
+        .rename(columns={"Home team":"Wins"})
+    return wins
 
 if __name__ == "__main__":
     # read the details from the google sheet
     formatedList = read_google_sheet()
     #create a dataframe
     df = createDatafrme(formatedList)
-    print(df)
+    formatedDf = formatDataframe(df)
+    print(formatedDf)
 
     #Prepare the dashboard
     st.title("FifaAnalytics")
 
+    st.dataframe(formatedDf)
+    st.dataframe(getWins(formatedDf))
+
     # 1.
+
+    fig = px.pie(getWins(formatedDf), values='Wins', names='winner', title='Win %')
+    st.plotly_chart(fig)
+
+    fig = px.bar(getWins(formatedDf), x="Date", y="Wins", color='winner')
+    st.plotly_chart(fig)
